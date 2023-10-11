a8f3da3540e11e8c1ca783d43a9ec004ac9851db
ytyaru <yttry0@gmail.com>
2023-10-11 11:15:57 +0900

    7 リサイズ画面分割（Gapサイズを--font-sizeにする）

---
+++ docs/7/js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    splitScreen.make()
+    splitScreen.make()
+    splitScreen.make({'count':1})
+    splitScreen.make({'count':3})
+    splitScreen.make({'writingMode':'vertical'})
+    splitScreen.make({'count':3, 'writingMode':'vertical'})
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
