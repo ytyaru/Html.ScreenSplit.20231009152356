9d4d2421ea49ca1fc13c057a1ae2e394f70eed4b
ytyaru <yttry0@gmail.com>
2023-10-12 20:10:58 +0900

    add 13

---
+++ docs/13/js/main.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    centerSplitScreen.make()
+    centerSplitScreen.make()
+    centerSplitScreen.make({'count':1})
+    centerSplitScreen.make({'count':3})
+    centerSplitScreen.make({'writingMode':'vertical'})
+    centerSplitScreen.make({'count':3, 'writingMode':'vertical'})
+    centerSplitScreen.make({'hasSplitter':false})
+    centerSplitScreen.make({'hasSplitter':false, 'writingMode':'vertical'})
+    centerSplitScreen.make({'hasSplitter':true, 'writingMode':'vertical'})
+    /*
+    */
+    window.addEventListener('resize', debounce((e)=>{centerSplitScreen.resize()},300))
+    /*
+    */
+    /*
+    splitScreen.make()
+    splitScreen.make()
+    splitScreen.make({'count':1})
+    splitScreen.make({'count':3})
+    splitScreen.make({'writingMode':'vertical'})
+    splitScreen.make({'count':3, 'writingMode':'vertical'})
+    window.addEventListener('resize', debounce((e)=>{splitScreen.resize()},300))
+    */
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
