64909df1bf443070abd566fb55e3672b5aced534
ytyaru <yttry0@gmail.com>
2023-10-16 19:15:23 +0900

    25 途中

---
+++ docs/25/js/responsive/fit-text-block.js
@@ -0,0 +1,260 @@
+(function() {
+class FitTextBlock {
+    constructor() {
+        this.pager = new Pager()
+        this.fitP = new FitParagraph()
+        this.clearBlock()
+    }
+    clearBlock() {
+        this.addBlockIdx=0; this.startIndex=0; this.logs={}; this.blocks=[]; this.tryHtml='';
+        this.size = `${document.documentElement.clientWidth}x${document.documentElement.clientHeight}`
+        this.logs[this.size] = [] // {'blockStartIndex':0, 'blockEndIndex':0, 'html':''}
+        this.init()
+    }
+    addBlock(block) {
+        if (!TextBlock.isIterd) { this.blocks.push(block) }
+        let startIndex = 0
+        Array.from(document.querySelectorAll('#dummy-screen .inner-screen')).map(screen=>screen.innerHTML='')
+        const screen = document.querySelector('#dummy-screen .inner-screen')
+
+        this.fitP.addBlock(block)
+        this.pager.setSplitterText()
+
+        // 最初の頁を表示する（未実装：前回復元時は所定頁をHTML表示する）
+        if (this.logs[this.size].length===this.#screenCount()) { this.init() }
+    }
+    addBlockEnd() { this.fitP.finish(); this.init(); }
+    #screenCount() { return Array.from(document.querySelectorAll('#screen .inner-screen')).length }
+    init() { this.pager.init(this.logs[this.size]); this.fitP.init(this.logs[this.size]); }
+    next() { this.pager.next() }
+    prev() { this.pager.prev() }
+    resize() { // this.blocksはすべて読込完了している前提。つまり完了するまではresizeを呼び出すべきではない。
+        this.size = `${document.documentElement.clientWidth}x${document.documentElement.clientHeight}`
+        if (!this.logs.hasOwnProperty(this.size)) { 
+            this.logs[this.size] = []
+            this.init()
+            this.fitP.resize()
+            for (let block of this.blocks) { this.addBlock(block) }
+            this.addBlockEnd()
+        } else { this.init(); this.fitP.resize(); } // 既存のlogs[size]を使い回す（再計算しない）
+    }
+}
+class Pager {
+    constructor() { this.page = 0; this.logs = null; }
+    #screenCount() { return Array.from(document.querySelectorAll('#screen .inner-screen')).length }
+    //init(logs) { this.page = 0; this.logs = logs; this.#set(); }
+    init(logs) { this.page = 0; this.logs = logs; this.#set(); console.log('Pager.init()', this.logs); }
+    #set() {
+        const screens = Array.from(document.querySelectorAll('#screen .inner-screen')) 
+        for (let i=0; i<screens.length; i++) {
+            const idx = (this.page * screens.length) + i
+            screens[i].innerHTML = (idx < this.logs.length) ? this.logs[idx].html : ''
+        }
+        //document.querySelector('#screen .splitter').textContent = `中央スプリッター　${this.page+1}/${this.logs.length}`
+        this.setSplitterText()
+    }
+    next() {
+        if (((this.page+1)*this.#screenCount()) < this.logs.length) {
+            this.page += 1
+            this.#set()
+        }
+    }
+    prev() {
+        if (0 < this.page) {
+            this.page -= 1
+            this.#set()
+        }
+    }
+    setSplitterText() {
+        centerSplitScreen.setSplitterText(`中央スプリッター　${this.page+1}/${Math.ceil(this.logs.length/this.#screenCount())}`)
+    }
+}
+class FitParagraph {
+    constructor() { this.logs=null; this.blocks=[]; this.tryHtml=''; this.startIndex=0; }
+    init(logs) { this.logs=logs; }
+    resize() { this.tryHtml=''; this.startIndex=0; }
+    addBlock(block) {
+        if (!TextBlock.isIterd) { this.blocks.push(block) }
+        let startIndex = 0
+        Array.from(document.querySelectorAll('#dummy-screen .inner-screen')).map(screen=>screen.innerHTML='')
+        const screen = document.querySelector('#dummy-screen .inner-screen')
+
+        const html = this.tryHtml
+        const blockHtml = blockParser.parse(block)
+        this.tryHtml += blockHtml
+        screen.innerHTML = this.tryHtml
+        if (this.#clientBlock() < this.#clientBlockEl(screen)) { // 一画面に収まらない
+            if (''===html) { throw new Error('この段落は一画面に収まりません。複数の段落に分けてください。') }
+            this.tryHtml = blockHtml
+            this.logs.push({'blockStartIndex':this.startIndex, 'blockEndIndex':this.blocks.length-1, 'html':html})
+            this.startIndex = this.blocks.length
+        }
+    }
+    finish() { this.logs.push({'blockStartIndex':this.startIndex, 'blockEndIndex':-1, 'html':this.tryHtml}); }
+    #isVertical() { return this.#writingMode().startsWith('vertical') }
+    #isHorizontal() { return this.#writingMode().startsWith('horizontal') }
+    #writingMode() { return Css.get('--writing-mode').trim().toLowerCase() }
+    #writingModeReverse() { return (this.#isVertical()) ? 'horizontal-tb' : 'vertical-rl' }
+    #clientBlock() { return (this.#isVertical()) ? this.#clientWidth() : this.#clientHeight() }
+    #clientBlockEl(el) { return el.getBoundingClientRect()[`${(this.#isVertical()) ? 'width' : 'height' }`] }
+    #clientInline() { return (this.#isVertical()) ? this.#clientHeight() : this.#clientWidth() }
+    #clientWidth() { return document.documentElement.clientWidth }
+    #clientHeight() { return document.documentElement.clientHeight }
+}
+class FitInlineElement {
+    constructor() { this.logs=null; this.blocks=[]; this.tryHtml=''; this.startIndex=0; }
+    init(logs) { this.logs=logs; }
+    resize() { this.tryHtml=''; this.startIndex=0; }
+    addBlock(block) {
+        if (!TextBlock.isIterd) { this.blocks.push(block) }
+        let startIndex = 0
+        Array.from(document.querySelectorAll('#dummy-screen .inner-screen')).map(screen=>screen.innerHTML='')
+        const screen = document.querySelector('#dummy-screen .inner-screen')
+
+        const html = this.tryHtml
+        const blockHtml = blockParser.parse(block)
+        this.tryHtml += blockHtml
+        screen.innerHTML = this.tryHtml
+        if (this.#clientBlock() < this.#clientBlockEl(screen)) { // 一画面に収まらない
+            if (''===html) { throw new Error('この段落は一画面に収まりません。複数の段落に分けてください。') }
+            this.tryHtml = blockHtml
+            this.logs.push({'blockStartIndex':this.startIndex, 'blockEndIndex':this.blocks.length-1, 'html':html})
+            this.startIndex = this.blocks.length
+        }
+    }
+    finish() { this.logs.push({'blockStartIndex':this.startIndex, 'blockEndIndex':-1, 'html':this.tryHtml}); }
+
+    /*
+    constructor() { this.logs=null; this.tryHtml=''; }
+    init(logs) { this.logs = logs; }
+    make(block) {
+        if (!TextBlock.isIterd) { this.blocks.push(block) }
+        let startIndex = 0
+        Array.from(document.querySelectorAll('#dummy-screen .inner-screen')).map(screen=>screen.innerHTML='')
+        const screen = document.querySelector('#dummy-screen .inner-screen')
+
+        // 一画面内に収まる
+        // 一画面内に収まらない
+
+        const html = this.tryHtml
+        const blockHtml = blockParser.parse(block)
+        this.tryHtml += blockHtml
+        screen.innerHTML = this.tryHtml
+        if (this.#clientBlock() < this.#clientBlockEl(screen)) { // 一画面に収まらない
+            // 段落内のどこまでなら一画面内に収まる？
+            // 一画面内に収まるようblockHtmlを分割する
+            // その数だけthis.logs[this.size].push({...})する
+
+
+            //if (''===html) { throw new Error('この段落は一画面に収まりません。複数の段落に分けてください。') }
+            if (''===html) { // 最初の段落がすでに一画面内に収まらない
+                // throw new Error('この段落は一画面に収まりません。複数の段落に分けてください。')
+            } else {
+                this.tryHtml = blockHtml
+                //this.logs[this.size].push({'blockStartIndex':this.startIndex, 'blockEndIndex':endIndex, 'html':html})
+                this.logs[this.size].push({'blockStartIndex':this.startIndex, 'blockEndIndex':this.blocks.length-1, 'html':html})
+                //this.startIndex = endIndex + 1
+                this.startIndex = this.blocks.length
+                centerSplitScreen.setSplitterText(`中央スプリッター　1/${this.logs[this.size].length}`)
+            }
+//            this.tryHtml = blockHtml
+//            return [html, this.blocks.length-1]
+        }
+//        return [null, -1]
+    }
+    finish() {
+        this.logs[this.size].push({'blockStartIndex':this.startIndex, 'blockEndIndex':-1, 'html':this.tryHtml})
+    }
+    */
+    #isVertical() { return this.#writingMode().startsWith('vertical') }
+    #isHorizontal() { return this.#writingMode().startsWith('horizontal') }
+    #writingMode() { return Css.get('--writing-mode').trim().toLowerCase() }
+    #writingModeReverse() { return (this.#isVertical()) ? 'horizontal-tb' : 'vertical-rl' }
+    #clientBlock() { return (this.#isVertical()) ? this.#clientWidth() : this.#clientHeight() }
+    #clientBlockEl(el) { return el.getBoundingClientRect()[`${(this.#isVertical()) ? 'width' : 'height' }`] }
+    #clientInline() { return (this.#isVertical()) ? this.#clientHeight() : this.#clientWidth() }
+    #clientWidth() { return document.documentElement.clientWidth }
+    #clientHeight() { return document.documentElement.clientHeight }
+}
+class SpanSplitter { // innerHTML内にあるテキストを一字ずつspanで囲む。ただしruby,em,aなどのインラインHTML要素はそのまま出力する。
+    split(html) {
+        // <???></???>, <??? />, <???>    HTMLタグには３パターンある。正規、XML、省略。<a></a>, <input/>, <input>等。今回は正規のみ
+        //html.matchAll(/(<[^<>]+>[^<>]+</([^<>]+)>)/g)
+        // let matches = html.matchAll(/(<[^\/<>]+>[^<>]+<\/[^\/<>]+>)/g)
+        // for (let match of matches) {console.log(match);}
+
+        const elIdxs = [] // {start:0, end:0}
+        let [startIdx, endIdx]  = [0, 0]
+        let name = null
+        //for (let c of html.Graphemes) {
+        for (let i=0; i<html.Graphemes.length; i++) {
+            let c = html.Graphemes[i]
+            let n = (i<html.Graphemes.length) ? html.Graphemes[i+1] : ''
+            if (name) { // </name>まで飛ばす
+                endIdx = this.#getEndTagIdx(html, i, name)
+                //endIdx = this.#getEndTagIdx(html, i-1, name)
+                console.log({'start':startIdx, 'end':endIdx})
+                elIdxs.push({'start':startIdx, 'end':endIdx})
+                i = endIdx
+                startIdx = i + 1
+            }
+            if ('<'===c && '/'!==n) { // 開始タグ
+                startIdx = i
+                console.log(this.#getName(html, i+1))
+                // [name, i] = this.#getName(html, i+1)
+                let [a, b] = this.#getName(html, i+1)
+                name = a
+                i = b
+                console.log(i, name)
+            }
+        }
+        console.log(elIdxs)
+        console.log(elIdxs[0])
+        return this.#join(html, elIdxs)
+    }
+    #getName(html, s) {
+        for (let i=s; i<html.Graphemes.length; i++) {
+            let c = html.Graphemes[i]
+            if ('>'===c) { return [html.slice(s, i).trim('/').trim(), i+1] }
+        }
+        return ['', -1]
+    }
+    #getEndTagIdx(html, s, name) {
+        console.log('getEndTagIndex:', s, name)
+        for (let i=s; i<html.Graphemes.length; i++) {
+            if ('<'!==html.Graphemes[i+0]) { continue }
+            if ('/'!==html.Graphemes[i+1]) { continue }
+            for (let n=0; n<name.length; n++) {
+                if (name[n]!==html.Graphemes[i+2+n]) { continue }
+            }
+            if ('>'!==html.Graphemes[i+2+name.length]) { continue }
+            //return i
+            //return i+2+name.length
+            return i+2+name.length+1
+        }
+    }
+    #join(html, elIdxs) {
+        let text = ''
+        let lastElIdx = null
+        for (let elIdx of elIdxs) {
+//            if (0 < elIdx.start) { text += html.Graphemes.slice(0, elIdx.start).join('') }
+            if (0 < elIdx.start) { text += this.#textToSpan(html.Graphemes.slice(0, elIdx.start)) }
+            text += html.Graphemes.slice(elIdx.start, elIdx.end).join('')
+            lastElIdx = elIdx
+        }
+        if (lastElIdx.end < html.length-1) {
+//            text += html.Graphemes.slice(lastElIdx.end).join('')
+            text += this.#textToSpan(html.Graphemes.slice(lastElIdx.end))
+        }
+        return text
+    }
+    #textToSpan(graphemes) {
+        let html = ''
+        for (let g of graphemes) { html += `<span>${g}</span>` }
+        return html
+    }
+}
+const spanSplitter = new SpanSplitter()
+console.log(spanSplitter.split('これはinnerHTML内テキストです。<em>HTMLタグ</em>も含みます。'))
+window.fitTextBlock = new FitTextBlock()
+})();
